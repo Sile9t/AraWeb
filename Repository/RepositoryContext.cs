6785f3ce381ed5286167417ac38489c522dfc88a
Sile9t <eyesless1jack@gmail.com>
2024-08-27 16:35:48 +0300

    Added RepositoryContext

---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,19 @@
+﻿using Entities;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Apartment>? Apartments{ get; set; }
+        public DbSet<ReservationDate>? ReservationDates { get; set; }
+        public DbSet<Occupancy>? Occupancies{ get; set; }
+    }
+}
