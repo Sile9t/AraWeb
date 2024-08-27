c5ab143a2b1a9783084df18f8403209caf06dfd1
Sile9t <eyesless1jack@gmail.com>
2024-08-27 23:07:36 +0300

    Implemented ReposirotyBase

--- Repository/RepositoryBase.cs
+++ Repository/RepositoryBase.cs
@@ -3,7 +3,7 @@ using System.Linq.Expressions;
 
 namespace Repository
 {
-    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
     {
         protected RepositoryContext RepositoryContext;
 
