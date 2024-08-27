c78ae7a4be2bdacc5aa817982335cf3132be2475
Sile9t <eyesless1jack@gmail.com>
2024-08-28 00:31:24 +0300

    Added OccupancyRepository

---
+++ Repository/OccupancyRepository.cs
@@ -0,0 +1,13 @@
+﻿using Contracts;
+using Entities.Models;
+
+namespace Repository
+{
+    public class OccupancyRepository : RepositoryBase<Occupancy>, IOccupancyRepository
+    {
+        public OccupancyRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+        }
+    }
+}
