38391e4d076025d4958c440853ec939606807a90
Sile9t <eyesless1jack@gmail.com>
2024-08-28 13:21:35 +0300

    Injected sqlcontext configuration

--- AraWeb/Extensions/ServiceExtensions.cs
+++ AraWeb/Extensions/ServiceExtensions.cs
@@ -28,10 +28,7 @@ namespace AraWeb.Extensions
             });
 
         public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
-            services.AddDbContext<RepositoryContext>(options =>
-            {
-                options.UseSqlServer(configuration.GetConnectionString("sqlConnection"))
-            });
+            services.AddSqlServer<RepositoryContext>(configuration.GetConnectionString("sqlConnection"));
 
         public static void ConfigureRepositoryManager(this IServiceCollection services) =>
             services.AddScoped<IRepositoryManager, RepositoryManager>();
