b334833ba5ed08b9235b58be94726ffe54af1779
Sile9t <eyesless1jack@gmail.com>
2024-08-28 13:15:09 +0300

    Added ServiceManager

--- Service/ServiceManager.cs
+++ Service/ServiceManager.cs
@@ -1,9 +1,30 @@
-﻿using Service.Contracts;
+﻿using Contracts;
+using Service.Contracts;
 
 namespace Service
 {
     public class ServiceManager : IServiceManager
     {
+        private readonly Lazy<IUserService> _userService;
+        private readonly Lazy<IApartmentService> _apartmentService;
+        private readonly Lazy<IReservationDateService> _reservationDateService;
+        private readonly Lazy<IOccupancyService> _occupancyService;
 
+        public ServiceManager(IRepositoryManager repositoryManager, ILoggerManager logger)
+        {
+            _userService = new Lazy<IUserService>(() => 
+                new UserService(repositoryManager, logger));
+            _apartmentService = new Lazy<IApartmentService>(() => 
+                new ApartmentService(repositoryManager, logger));
+            _reservationDateService = new Lazy<IReservationDateService>(() => 
+                new ReservationService(repositoryManager, logger));
+            _occupancyService = new Lazy<IOccupancyService>(() => 
+                new OccupancyService(repositoryManager, logger));
+        }
+
+        public IUserService UserService => _userService.Value;
+        public IApartmentService ApartmentService => _apartmentService.Value;
+        public IReservationDateService ReservationDateService => _reservationDateService.Value;
+        public IOccupancyService OccupancyService => _occupancyService.Value;
     }
 }
