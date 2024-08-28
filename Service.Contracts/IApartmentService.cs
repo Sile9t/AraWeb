b8a973d0534d39725a6093029a39975d2a4cfb7a
Sile9t <eyesless1jack@gmail.com>
2024-08-28 14:54:11 +0300

    Added GetAllApartments to ApartmentService

--- Service.Contracts/IApartmentService.cs
+++ Service.Contracts/IApartmentService.cs
@@ -1,6 +1,9 @@
-﻿namespace Service.Contracts
+﻿using Shared.Dtos;
+
+namespace Service.Contracts
 {
     public interface IApartmentService
     {
+        IEnumerable<ApartmentDto> GetAllApartments(bool trackChanges);
     }
 }
--- Service/ApartmentService.cs
+++ Service/ApartmentService.cs
@@ -1,5 +1,6 @@
 ﻿using Contracts;
 using Service.Contracts;
+using Shared.Dtos;
 
 namespace Service
 {
@@ -12,6 +13,15 @@ namespace Service
         {
             _repository = repository;
             _logger = logger;
+        }
+
+        public IEnumerable<ApartmentDto> GetAllApartments(bool trackChanges)
+        {
+            var apartments = _repository.Apartment.GetAllApartments(trackChanges);
+            if (apartments is null)
+                throw new Exception();
+
+            return apartments;
         }
     }
 }
