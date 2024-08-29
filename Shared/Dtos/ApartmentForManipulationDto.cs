b94d70b2acab5f5463e93727b91180b9dd95b902
Sile9t <eyesless1jack@gmail.com>
2024-08-29 12:08:48 +0300

    Added ApartmentForManipulationDto

--- Shared/Dtos/ApartmentForManipulationDto.cs
+++ Shared/Dtos/ApartmentForManipulationDto.cs
@@ -6,25 +6,25 @@ namespace Shared.Dtos
     public abstract record ApartmentForManipulationDto
     {
         [MaxLength(80, ErrorMessage = "Maximum length for Name is 80 characters")]
-        public string? Name { get; set; }
+        public string? Name { get; init; }
 
         [Required(ErrorMessage = "Address field is required!")]
         [MinLength(10, ErrorMessage = "Minimum length for Address is 10 characters")]
-        public string? Address { get; set; }
+        public string? Address { get; init; }
 
         [Required(ErrorMessage = "Capacity square field is required!")]
-        public double CapacitySquare { get; set; }
+        public double CapacitySquare { get; init; }
 
         [Required(ErrorMessage = "Guests count field is required!")]
-        public int GuestsCount { get; set; }
+        public int GuestsCount { get; init; }
 
         [Required(ErrorMessage = "Beds count field is required!")]
-        public int BedsCount { get; set; }
+        public int BedsCount { get; init; }
 
         [Required(ErrorMessage = "Rooms count field is required!")]
-        public int RoomsCount { get; set; }
+        public int RoomsCount { get; init; }
 
-        public double Rate { get; set; }
-        public long ReviewsCount { get; set; }
+        public double Rate { get; init; }
+        public long ReviewsCount { get; init; }
     }
 }
