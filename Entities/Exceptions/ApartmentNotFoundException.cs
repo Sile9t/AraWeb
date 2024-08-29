2c7253b8bd14fb2e634e6df3f75d6452fdaed560
Sile9t <eyesless1jack@gmail.com>
2024-08-29 11:23:32 +0300

    Added ApartmentNotFoundException

---
+++ Entities/Exceptions/ApartmentNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities.Exceptions
+{
+    public class ApartmentNotFoundException : NotFoundException
+    {
+        public ApartmentNotFoundException(Guid id)
+            : base($"Apartment with id: {id} doesn't exist in the database.")
+        {
+        }
+    }
+}
