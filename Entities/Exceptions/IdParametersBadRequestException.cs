3e9cf185ee746da840f57688ef24fd236e93bf2c
Sile9t <eyesless1jack@gmail.com>
2024-08-29 14:41:10 +0300

    Added CollectionByIdsBadRequestException

--- Entities/Exceptions/IdParametersBadRequestException.cs
+++ Entities/Exceptions/IdParametersBadRequestException.cs
@@ -7,4 +7,12 @@
         {
         }
     }
+
+    public sealed class CollectionByIdsBadRequestException : BadRequestException
+    {
+        public CollectionByIdsBadRequestException()
+            : base("Collection count mismatch comparing to ids.")
+        {
+        }
+    }
 }
