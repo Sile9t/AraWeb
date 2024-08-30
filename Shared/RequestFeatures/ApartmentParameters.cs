e59ab87cd697da162248951f98e8a17b2b1b5d37
Sile9t <eyesless1jack@gmail.com>
2024-08-30 15:51:33 +0300

    Expanded  ApartmentParameters

--- Shared/RequestFeatures/ApartmentParameters.cs
+++ Shared/RequestFeatures/ApartmentParameters.cs
@@ -2,5 +2,36 @@
 {
     public class ApartmentParameters : RequestParameters
     {
+        public double MinSquare { get; set; }
+        public double MaxSquare { get; set; } = uint.MaxValue;
+
+        public bool ValidSquareRange => MaxSquare > MinSquare;
+
+        public uint MinGuestsCount {  get; set; }
+        public uint MaxGuestsCount { get; set; } = uint.MaxValue;
+
+        public bool ValidGuestsRange => MaxGuestsCount > MinGuestsCount;
+
+        public uint MinBedsCount { get; set; }
+        public uint MaxBedsCount { get; set; } = uint.MaxValue
+
+        public bool ValidBedsRange => MaxBedsCount > MinBedsCount;
+
+        public uint MinRoomsCount { get; set; }
+        public uint MaxRoomsCount { get; set; } = uint.MaxValue
+
+        public bool ValidRoomsRange => MaxRoomsCount > MinRoomsCount;
+
+        public double MinRate { get; set; }
+        public double MaxRate { get; set; } = 10;
+
+        public bool ValidRateRange => MaxRate > MinRate;
+
+        public long MinReviewsCount { get; set; }
+        public long MaxReviewsCount { get; set; } = long.MaxValue;
+
+        public bool ValidReaviewsCountRange => MaxReviewsCount > MinReviewsCount;
+
+        public string? SearchTerm { get; set; }
     }
 }
