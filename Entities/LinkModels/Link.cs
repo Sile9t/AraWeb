98118e44922a3f4728390da3d9c5bae5c2cfa30c
Sile9t <eyesless1jack@gmail.com>
2024-08-30 17:31:26 +0300

    Added Link model

---
+++ Entities/LinkModels/Link.cs
@@ -0,0 +1,20 @@
+﻿namespace Entities.LinkModels
+{
+    public class Link
+    {
+        public string? Href;
+        public string? Rel;
+        public string? Method;
+
+        public Link()
+        {
+        }
+
+        public Link(string href, string rel, string method)
+        {
+            Href = href;
+            Rel = rel;
+            Method = method;
+        }
+    }
+}
