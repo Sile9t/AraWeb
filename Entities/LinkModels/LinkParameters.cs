fadb96b555582dcf6038017ea9ad80621eaed93d
Sile9t <eyesless1jack@gmail.com>
2024-08-30 18:55:46 +0300

    Added LinkParameters class

---
+++ Entities/LinkModels/LinkParameters.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Shared.RequestFeatures;
+
+namespace Entities.LinkModels
+{
+    public record LinkParameters
+    {
+        ApartmentParameters ApartmentParameters { get; init; }
+        HttpContext HttpContext { get; init; }
+    }
+}
