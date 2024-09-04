a3156d5b4e1274d62a729abba8203400fe998cf9
Sile9t <eyesless1jack@gmail.com>
2024-09-04 21:43:23 +0300

    Refactored error messages for user dto model

--- Shared/Dtos/UserForManipulationDto.cs
+++ Shared/Dtos/UserForManipulationDto.cs
@@ -8,9 +8,9 @@ namespace Shared.Dtos
         public string? LastName { get; init; }
         [Required(ErrorMessage = "UserName is required")]
         public string? UserName { get; init; }
-        [Required(ErrorMessage = "UserName is required")]
+        [Required(ErrorMessage = "Password is required")]
         public string? Password { get; init; }
-        [Required(ErrorMessage = "UserName is required")]
+        [Required(ErrorMessage = "Phone number is required")]
         public string? PhoneNumber { get; init; }
         public ICollection<string>? Roles { get; init; }
     }
