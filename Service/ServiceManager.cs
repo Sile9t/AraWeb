602c40d610f78321cf6b33dd7d790ea692df5885
Sile9t <eyesless1jack@gmail.com>
2024-09-05 10:30:22 +0300

    Injected AuthenticationService to ServiceManager

--- Service/ServiceManager.cs
+++ Service/ServiceManager.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
 using Contracts;
 using Contracts.Links;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Service.Contracts;
 using Shared.Dtos;
 
@@ -9,15 +12,19 @@ namespace Service
     public class ServiceManager : IServiceManager
     {
         private readonly Lazy<IUserService> _userService;
+        private readonly Lazy<IAuthenticationService> _authenticationService;
         private readonly Lazy<IApartmentService> _apartmentService;
         private readonly Lazy<IReservationDateService> _reservationDateService;
         private readonly Lazy<IOccupancyService> _occupancyService;
 
         public ServiceManager(IRepositoryManager repositoryManager, ILoggerManager logger,
-            IMapper mapper, IApartmentLinks apartmentLinks)
+            IMapper mapper, IApartmentLinks apartmentLinks, UserManager<User> userManager,
+            IConfiguration configuration)
         {
             _userService = new Lazy<IUserService>(() => 
                 new UserService(repositoryManager, logger));
+            _authenticationService= new Lazy<IAuthenticationService>(() =>
+                new AuthenticationService(logger, mapper, userManager, configuration));
             _apartmentService = new Lazy<IApartmentService>(() => 
                 new ApartmentService(repositoryManager, logger, mapper, apartmentLinks));
             _reservationDateService = new Lazy<IReservationDateService>(() => 
@@ -27,6 +34,7 @@ namespace Service
         }
 
         public IUserService UserService => _userService.Value;
+        public IAuthenticationService AuthenticationService => _authenticationService.Value;
         public IApartmentService ApartmentService => _apartmentService.Value;
         public IReservationDateService ReservationDateService => _reservationDateService.Value;
         public IOccupancyService OccupancyService => _occupancyService.Value;
