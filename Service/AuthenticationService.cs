0f61d09c73b068a805b7f21f68715d3656a15c7f
Sile9t <eyesless1jack@gmail.com>
2024-09-08 10:27:38 +0300

    Added refresh token genaration and validation

--- Service/AuthenticationService.cs
+++ Service/AuthenticationService.cs
@@ -8,6 +8,7 @@ using Service.Contracts;
 using Shared.Dtos;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace Service
@@ -54,18 +55,29 @@ namespace Service
             return result;
         }
 
-        public async Task<string> CreateToken()
+        public async Task<TokenDto> CreateToken(bool populateExp)
         {
             var signingCredentials = GetSigningCredentials();
             var claims = await GetClaims();
             var tokenOptions = GenerateTokenOptions(signingCredentials, claims);
+
+            var refreshToken = GenerateRefreshToken();
+
+            _user!.RefreshToken = refreshToken;
+
+            if (populateExp)
+                _user.RefreshTokenExperyTime = DateTime.Now.AddDays(1);
+
+            await _userManager.UpdateAsync(_user);
+
+            var accessToken = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
 
             return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
         }
 
         private SigningCredentials GetSigningCredentials()
         {
-            var key = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("SECRET"));
+            var key = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("SECRET")!);
             var secret = new SymmetricSecurityKey(key);
 
             return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
@@ -100,6 +112,45 @@ namespace Service
                 signingCredentials: signingCredentials
             );
             return tokenOptions;
+        }
+
+        private string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomNumber);
+                return Convert.ToBase64String(randomNumber);
+            }
+        }
+
+        private ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+        {
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = true,
+                ValidateIssuer = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                    Environment.GetEnvironmentVariable("SECRET")!)),
+                ValidateLifetime = true,
+                ValidIssuer = jwtSettings["validIssuer"],
+                ValidAudience = jwtSettings["validAudience"]
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken securityToken;
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters,
+                out securityToken);
+
+            var jwtSecurityToken = securityToken as JwtSecurityToken;
+            if (jwtSecurityToken is null || !jwtSecurityToken.Header.Alg.Equals(
+                SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+                throw new SecurityTokenException("Invalid token");
+
+            return principal;
         }
     }
 }
--- Shared/Dtos/TokenDto.cs
+++ Shared/Dtos/TokenDto.cs
@@ -2,6 +2,12 @@
 {
     public record TokenDto
     {
+        public TokenDto(string accessToken, string refreshToke)
+        {
+            AccessToken = accessToken;
+            RefreshToken = refreshToke;
+        }
+
         public string AccessToken { get; init; }
         public string RefreshToken { get; init; }
     }
