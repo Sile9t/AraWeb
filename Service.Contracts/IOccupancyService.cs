8bca3a141cb29273c3bc90d833cbe76826c27255
Sile9t <eyesless1jack@gmail.com>
2024-09-11 12:30:42 +0300

    Added occupancy dtos

--- Service.Contracts/IOccupancyService.cs
+++ Service.Contracts/IOccupancyService.cs
@@ -1,6 +1,13 @@
-﻿namespace Service.Contracts
+﻿using Shared.Dtos;
+
+namespace Service.Contracts
 {
     public interface IOccupancyService
     {
+        Task<OccupancyDto> GetAllOccupanciesAsync(bool trackChanges);
+        Task<OccupancyDto> GetOccupanciesForUserAsync(Guid userId, bool trackChanges);
+        Task<OccupancyDto> GetOccupanciesForApartmentAsync(Guid apartId, bool trackChanges);
+        Task<OccupancyDto> CreateOccupancyForApartmentAsync(Guid apartId, 
+            OccupancyForCreationDto occupancyDto, bool trackChanges);
     }
 }
---
+++ Shared/Dtos/OccupancyForManipulationDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Dtos
+{
+    public abstract record OccupancyForManipulationDto
+    {
+        public DateTime CreatedOn { get; init; } = DateTime.Now;
+        public DateTime OccupancyDate { get; init; }
+        public DateTime EvictionDate { get; init; }
+
+        public decimal TotalCost { get; init; }
+        public int OccupStateId { get; init; } = 0;
+
+        public string? ReservedById { get; set; }
+        public Guid? ApartmentId { get; set; }
+    }
+
+    public record OccupancyForCreationDto : OccupancyForManipulationDto;
+    public record OccupancyForUpdateDto : OccupancyForManipulationDto;
+}
